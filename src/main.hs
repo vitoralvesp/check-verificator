533da57cbbaab3e1a9349569e3036f683bd94d50
vitoralvesp <pereira.alves.vitor@outlook.com>
2025-03-29 15:23:41 -0300

    Estratégia para Verificar se o Rei Branco está em Xeque

--- src/main.hs
+++ src/main.hs
@@ -1,22 +1,20 @@
 {- TODO 
- - Ler a Notação de Forsyth:
+ - Ler a Notação de Forsyth
    - Formato da Entrada: [Peças Pretas, Peões das Peças Pretas, Linhas entre as Peças, Peões das Peças Brancas, Peças Brancas]
- - Realizar a leitura da notação e converter em uma matriz
+ - Realizar a leitura da notação e converter em uma matriz 8 x 8
  - Identificar a posição do rei branco na matriz
  - Verificar se alguma peça preta pode atacá-lo
- - Retornar True ou False para os casos
+ - Retornar True se alguma peça pode atacá-lo, ou False caso contrário
  -}
 
-{-
-check_verificator :: [String] -> Bool
-check_verificator [] = False
-check_verificator lst = True
--}
+-- Verifica se o Rei Branco está em Xeque
+check_verificator :: [[Int]] -> Bool
+check_verificator _[[]] = False
+check_verificator chess_board = True
 
-matrix :: Int -> Int -> [String] -> [[Char]]
-matrix x y
-     | [x][y] = 
+-- Leitura da Notação de Forsyth em uma Matriz
+forsyth_mtrx :: [String] -> [[Int]]
+forsyth_mtrx _[] = []
+forsyth_mtrx forsyth_notation = [[1,2,3]]
 
-main :: IO()
-main = do
-       putStr "Executando..."
+
