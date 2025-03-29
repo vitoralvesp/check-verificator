08231618070d2b7f8aa234b07718316d82e5b1b2
L. Sarai <sarai.gg.lucas@gmail.com>
2025-03-29 18:51:07 -0300

    tentando achar posicao

--- src/main.hs
+++ src/main.hs
@@ -24,5 +24,20 @@ chess_board = map (concatMap forsyth_notation)
        | character >= '1' && character <= '8' = replicate (read [character]) '.'
        | otherwise = [character]
          
+charParaInt :: Char -> Int
+charParaInt c = read [c] :: Int
+
+
+procura_peca :: Char -> String -> Int
+procura_peca peca_procurada lst = procura peca_procurada 0 lst
+
+
+procura :: Char -> Int -> String -> Int
+procura peca_procurada pos (elemento:(prox_elemento:resto))
+        | elemento == peca_procurada = pos
+        | elemento >= '1' && elemento <= '8' = procura peca_procurada (pos+ (charParaInt elemento)) (prox_elemento:resto) 
+        | otherwise = procura peca_procurada (pos+1) (prox_elemento:resto)
+  
+
 
 
