75e8c54787f20c9c328058fd6dd2dbe613e131be
vitoralvesp <pereira.alves.vitor@outlook.com>
2025-04-01 14:35:21 -0300

    Alteração na documentação de algumas funções

--- src/main.hs
+++ src/main.hs
@@ -1,24 +1,22 @@
 {- TODO 
-
-Peças = torre; cavalo; bispo; dama; rei; peão
-
-Algoritmo:
-- Procura peças pretas e retorna posição por linha
-- Faz trajetos possíveis
-- Verifica se alguma alcança rei branco
-verificaCheque = verificaTorre || verificaCavalo || verificaBispo || verificaDama || verificaReiPreto || verificaPeão
+ - Peças = torre; cavalo; bispo; dama; rei; peão
+ - Algoritmo:
+   - Procura peças pretas e retorna posição por linha
+   - Faz trajetos possíveis
+   - Verifica se alguma alcança rei branco
+   - verificaCheque = verificaTorre || verificaCavalo || verificaBispo || verificaDama || verificaReiPreto || verificaPeão
 	Se um for verdadeiro (se um já colocar em xeque), o programa retorna verdadeiro. Caso contrário (nenhum xeque), retorna falso
 
-Separar tarefas:
-	Lucas: Torre
-	Vitor: Bispo
-	Jéssica: Peão
+ - Tarefas:
+   - Lucas: Torre
+   - Vitor: Bispo
+   - Jéssica: Peão
 
-Casos base: peão, torre e bispo
-
-Dama = Bispo + Torre
-Rei = peão + Bispo
-Cavalo = 8 movimentos:
+ - Casos base: peão, torre e bispo
+ - Movimentos:
+   - Dama = Bispo + Torre
+   - Rei = peão + Bispo
+   - Cavalo = 8 movimentos:
       L cima/direita
       L cima/esquerda
       L direita/cima
@@ -26,14 +24,13 @@ Cavalo = 8 movimentos:
       L baixo/direita
       L baixo/esquerda
       L esquerda/cima
-      L esquerda/baixo 
-
+      L esquerda/baixo
  -}
+	
 
 -- Verifica se o Rei Branco está em Xeque
-check_verificator :: [[Int]] -> Bool
-check_verificator [[]] = False
-check_verificator board = True
+-- esta_em_xeque
+
 
 {- 
  - Leitura da Notação de Forsyth em uma Matriz
@@ -46,14 +43,23 @@ chess_board = map (concatMap forsyth_notation)
      forsyth_notation character
        | character >= '1' && character <= '8' = replicate (read [character]) '.'
        | otherwise = [character]
-         
+
+{-
+ - Leitura de um caractere para o tipo inteiro
+ -}
 charParaInt :: Char -> Int
 charParaInt c = read [c] :: Int
 
+
+{-
+ - Busca uma peça no tabuleiro e retorna a sua posição na linha
+ -}
 procura_peca :: Char -> [Char] -> Int
-procura_peca peca_procurada lst= procura peca_procurada 0 lst
+procura_peca peca_procurada lst = procura peca_procurada 0 lst
 
-
+{-
+ - Função complementar à procura_peca para realizar a busca da peça no tabuleiro 
+ -}
 procura :: Char -> Int -> [Char] -> Int
 procura _ _ [] = -1
 procura peca_procurada pos [elemento]
