4420894876394bac0f0bfd9023e94dde1de646c9
vitoralvesp <pereira.alves.vitor@outlook.com>
2025-04-01 20:54:32 -0300

    Nova solução

--- src/main.hs
+++ src/main.hs
@@ -1,109 +1,112 @@
-{- TODO 
- - Peças = torre; cavalo; bispo; dama; rei; peão
- - Algoritmo:
-   - Procura peças pretas e retorna posição por linha
-   - Faz trajetos possíveis
-   - Verifica se alguma alcança rei branco
-   - verificaCheque = verificaTorre || verificaCavalo || verificaBispo || verificaDama || verificaReiPreto || verificaPeão
-	Se um for verdadeiro (se um já colocar em xeque), o programa retorna verdadeiro. Caso contrário (nenhum xeque), retorna falso
+-- Interpreta a notação de forsyth em uma matriz que representa o tabuleiro de xadrez
+criar_tabuleiro :: [String] -> [[Char]]
+criar_tabuleiro = map (concatMap notacao_de_forsyth)
+  where
+    notacao_de_forsyth caractere
+      | caractere >= '1' && caractere <= '8' = replicate (read [caractere]) '.'
+      | otherwise = [caractere]
 
- - Tarefas:
-   - Lucas: Torre
-   - Vitor: Bispo
-   - Jéssica: Peão
+-- Seleciona todas as peças pretas e armazena as suas coordenadas
+selecionar_pecas :: [[Char]] -> [(Char, (Int, Int))]
+selecionar_pecas tabuleiro = selecionar_pecas_aux tabuleiro 0 []
 
- - Casos base: peão, torre e bispo
- - Movimentos:
-   - Dama = Bispo + Torre
-   - Rei = peão + Bispo
-   - Cavalo = 8 movimentos:
-      L cima/direita
-      L cima/esquerda
-      L direita/cima
-      L direita/baixo
-      L baixo/direita
-      L baixo/esquerda
-      L esquerda/cima
-      L esquerda/baixo
- -}
-	
+-- Função auxiliar que percorre o tabuleiro linha por linha
+selecionar_pecas_aux :: [[Char]] -> Int -> [(Char, (Int, Int))] -> [(Char, (Int, Int))]
+selecionar_pecas_aux [] _ resultado = resultado
+selecionar_pecas_aux (linha:restante) y resultado =
+    selecionar_pecas_aux restante (y + 1) (resultado ++ selecionar_peca_linha linha y 0 [])
 
--- Verifica se o Rei Branco está em Xeque
--- esta_em_xeque
+-- Função auxiliar que percorre uma linha do tabuleiro
+selecionar_peca_linha :: [Char] -> Int -> Int -> [(Char, (Int, Int))] -> [(Char, (Int, Int))]
+selecionar_peca_linha [] _ _ resultado = resultado
+selecionar_peca_linha (peca:restante) y x resultado
+    | peca >= 'a' && peca <= 'z' = selecionar_peca_linha restante y (x + 1) (resultado ++ [(peca, (x, y))])
+    | otherwise = selecionar_peca_linha restante y (x + 1) resultado
+
+-- Movimentos
+movimento_vertical_para_baixo :: (Int, Int) -> Int -> Int -> [[Char]] -> Bool
+movimento_vertical_para_baixo (x, y) capacidade_de_movimento contador_de_movimento tabuleiro
+                  | y > capacidade_de_movimento = False
+                  | tabuleiro !! y !! x == 'R' = True
+                  | tabuleiro !! y !! x /= '.' && contador_de_movimento > 0 = False
+                  | otherwise = movimento_vertical_para_baixo (x, y + 1) capacidade_de_movimento (contador_de_movimento+1) tabuleiro
+
+movimento_vertical_para_cima :: (Int, Int) -> Int -> Int -> [[Char]] -> Bool
+movimento_vertical_para_cima (x, y) capacidade_de_movimento contador_de_movimento tabuleiro
+                  | y < 0 = False
+                  | tabuleiro !! y !! x == 'R' = True
+                  | tabuleiro !! y !! x /= '.' && contador_de_movimento > 0 = False
+                  | otherwise = movimento_vertical_para_cima (x, y - 1) capacidade_de_movimento (contador_de_movimento+1) tabuleiro
+
+movimento_horizontal_para_a_direita :: (Int, Int) -> Int -> Int -> [[Char]] -> Bool
+movimento_horizontal_para_a_direita (x, y) capacidade_de_movimento contador_de_movimento tabuleiro
+                  | x > capacidade_de_movimento = False
+                  | tabuleiro !! y !! x == 'R' = True
+                  | tabuleiro !! y !! x /= '.' && contador_de_movimento > 0 = False
+                  | otherwise = movimento_horizontal_para_a_direita (x + 1, y) capacidade_de_movimento (contador_de_movimento+1) tabuleiro
+
+movimento_horizontal_para_a_esquerda :: (Int, Int) -> Int -> Int -> [[Char]] -> Bool
+movimento_horizontal_para_a_esquerda (x, y) capacidade_de_movimento contador_de_movimento tabuleiro
+                  | x < 0 = False
+                  | tabuleiro !! y !! x == 'R' = True
+                  | tabuleiro !! y !! x /= '.' && contador_de_movimento > 0 = False
+                  | otherwise = movimento_horizontal_para_a_esquerda (x - 1, y) capacidade_de_movimento (contador_de_movimento+1) tabuleiro
 
 
-{- 
- - Leitura da Notação de Forsyth em uma Matriz
- - [String]: notação de forsyth
- - [[Char]]: matriz 8x8 no formato de um tabuleiro de xadrez
- -}
-chess_board :: [String] -> [[Char]]
-chess_board = map (concatMap forsyth_notation)
-     where 
-     forsyth_notation character
-       | character >= '1' && character <= '8' = replicate (read [character]) '.'
-       | otherwise = [character]
+-- Peças
+torre :: (Char, (Int, Int)) -> [[Char]] -> Bool
+torre (peca, (x, y)) tabuleiro
+      | movimento_vertical_para_baixo (x, y) 7 0 tabuleiro 
+      || movimento_vertical_para_cima (x, y) 7 7 tabuleiro 
+      || movimento_horizontal_para_a_direita (x, y) 7 0 tabuleiro 
+      || movimento_horizontal_para_a_esquerda (x, y) 7 7 tabuleiro = True
+      | otherwise = False
 
-{-
- - Leitura de um caractere para o tipo inteiro
- -}
-charParaInt :: Char -> Int
-charParaInt c = read [c] :: Int
+cavalo :: (Char, (Int, Int)) -> Bool
+cavalo (peca, (x, y)) = False  
 
+bispo :: (Char, (Int, Int)) -> Bool
+bispo (peca, (x, y)) = False  
 
-{-
- - Busca uma peça no tabuleiro e retorna a sua posição na linha
- -}
-procura_peca :: Char -> [Char] -> Int
-procura_peca peca_procurada lst = procura peca_procurada 0 lst
+rainha :: (Char, (Int, Int)) -> Bool
+rainha (peca, (x, y)) = False  
 
-{-
- - Função complementar à procura_peca para realizar a busca da peça no tabuleiro 
- -}
-procura :: Char -> Int -> [Char] -> Int
-procura _ _ [] = -1
-procura peca_procurada pos [elemento]
-        | elemento == peca_procurada = pos
-        | otherwise = -1
-procura peca_procurada pos (elemento:(prox_elemento:resto))
-        | elemento == peca_procurada = pos
-        | elemento >= '1' && elemento <= '8' = procura peca_procurada (pos+ (charParaInt elemento)) (prox_elemento:resto) 
-        | otherwise = procura peca_procurada (pos+1) (prox_elemento:resto)
+rei :: (Char, (Int, Int)) -> Bool
+rei (peca, (x, y)) = False  
 
--- Verifica a linha da torre
-verifica_torre_linha :: String -> Bool
-verifica_torre_linha [] = False
-verifica_torre_linha (a:xs) = verifica_torre_linha_aux 0 pos_torre posicao_rei (a:xs)
-        where 
-                posicao_rei = procura_peca 'R' (a:xs)
-                pos_torre = procura_peca 't' (a:xs)
+peao :: (Char, (Int, Int)) -> Bool
+peao (peca, (x, y)) = False  
 
-verifica_torre_linha_aux :: Int -> Int -> Int -> String -> Bool
-verifica_torre_linha_aux _ _ _ [] = False
-verifica_torre_linha_aux ac pos_torre pos_rei (a:xs)
-        | pos_rei == -1 = False
-        | (a >= '1' && a <= '8') && (((pos_rei + (charParaInt a)+1) == pos_torre) || ((pos_rei - (charParaInt a)+1) == pos_torre))= True
-        | a >= '1' && a <= '8' = verifica_torre_linha_aux (ac + (charParaInt a)) pos_torre pos_rei xs
-        | ((ac > pos_torre && ac < pos_rei) || (ac > pos_rei && ac < pos_torre))= False 
-        | (a == 'R' && pos_rei > pos_torre) || (pos_rei == pos_torre+1) || (pos_rei == pos_torre-1) = True
-        | otherwise = verifica_torre_linha_aux (ac+1) pos_torre pos_rei xs
+-- Verifica se o Rei Branco está em xeque
+esta_em_xeque :: [String] -> Bool
+esta_em_xeque notacao_de_forsyth = esta_em_xeque_aux (selecionar_pecas (criar_tabuleiro notacao_de_forsyth)) (criar_tabuleiro notacao_de_forsyth)
 
--- Verifica a coluna da torre
+esta_em_xeque_aux :: [(Char, (Int, Int))] -> [[Char]] -> Bool
+esta_em_xeque_aux [] _ = False
+esta_em_xeque_aux ((peca, (x, y)):restante) tabuleiro
+  | peca == 't' = torre (peca, (x, y)) tabuleiro || esta_em_xeque_aux restante tabuleiro
+  | otherwise = esta_em_xeque_aux restante tabuleiro
 
 {-
-        FIXME: se houver peça na coluna da torre entre a torre e o rei, deve retornar falso, não verdadeiro.
+esta_em_xeque_aux :: [(Char, (Int, Int))] -> [[Char]] -> Bool
+esta_em_xeque_aux [] _ = False
+esta_em_xeque_aux ((peca, (x, y)):restante) tabuleiro
+  | peca == 't' = torre (peca, (x, y)) tabuleiro || esta_em_xeque_aux restante tabuleiro
+  | peca == 'c' = cavalo (peca, (x, y)) || esta_em_xeque_aux restante tabuleiro
+  | peca == 'b' = bispo (peca, (x, y)) || esta_em_xeque_aux restante tabuleiro
+  | peca == 'd' = rainha (peca, (x, y)) || esta_em_xeque_aux restante tabuleiro
+  | peca == 'r' = rei (peca, (x, y)) || esta_em_xeque_aux restante tabuleiro
+  | peca == 'p' = peao (peca, (x, y)) || esta_em_xeque_aux restante tabuleiro
+  | otherwise = esta_em_xeque_aux restante tabuleiro
 -}
-verifica_torre_coluna :: Int -> [String] -> Bool
-verifica_torre_coluna _ [] = False
-verifica_torre_coluna pos_torre (a:xs) = verifica_torre_coluna_prox_linhas pos_torre a || verifica_torre_coluna pos_torre xs
-
-verifica_torre_coluna_prox_linhas :: Int -> String -> Bool
-verifica_torre_coluna_prox_linhas pos_torre lst = verifica_torre_coluna_prox_linhas_aux 0 pos_torre lst
-
-verifica_torre_coluna_prox_linhas_aux :: Int -> Int -> String -> Bool
-verifica_torre_coluna_prox_linhas_aux _ _ [] = False  
-verifica_torre_coluna_prox_linhas_aux ac pos_torre (a:xs)
-    | ac == pos_torre = (a == 'R')
-    | ac > pos_torre = False  
-    | a >= '1' && a <= '8' = verifica_torre_coluna_prox_linhas_aux (ac + (charParaInt a)+1) pos_torre xs  
-    | otherwise = verifica_torre_coluna_prox_linhas_aux (ac + 1) pos_torre xs
+--
+main :: IO()
+main = do
+  putStr "\nTabuleiro interpretado da Notação Forsyth: "
+  print (criar_tabuleiro ["tcbdrbct","pppppppp","8","t7R","8","8","PPPPPPPP","TCBDRBCT"])
+  
+  putStr "\nSeleção das Peças: "
+  print (selecionar_pecas ["tcbdrbct","pppppppp","........","t......R","........","........","PPPPPPPP","TCBDRBCT"])
+  
+  putStr "\nEstá em Xeque? "
+  print (esta_em_xeque ["tcbdrbct","pppppppp","8","8","8","8","PPPPPPPP","TCBDRBCT"])
