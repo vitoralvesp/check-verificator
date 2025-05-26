6c27440207f6f1c5cd9c0b63b3d7a57cc20d30dc
vitoralvesp <pereira.alves.vitor@outlook.com>
2025-05-26 17:50:44 -0300

    Processamento da notação de forsyth (só aceita maiúsculas com aspas simples, por enquanto)

--- prolog-solution/src/main.pl
+++ prolog-solution/src/main.pl
@@ -16,16 +16,41 @@
 % Parâmetros: Lista, Lista
 criar_tabuleiro([], []).
 criar_tabuleiro([Linha|Resto], [LinhaInterpretada|TabuleiroInterpretado]) :-
-    converter_linha(Linha, LinhaInterpretada),
+    processar_linha(Linha, LinhaInterpretada),
     criar_tabuleiro(Resto, TabuleiroInterpretado).
 
 % Descrição: Converte os números (1 a 8) em '.', mas mantém as listas como estão
 % Parâmetros: Número (1 a 8) ou Caractere (Peças Pretas ou Brancas), Lista
-converter_linha(N, Lista) :-
+processar_linha(N, Lista) :-
     number(N),
     gerar_vazias(N, Lista).
-converter_linha(Lista, Lista) :-
-  	is_list(Lista).
+processar_linha(Linha, LinhaInterpretada) :-
+    is_list(Linha),
+    expandir_linha(Linha, LinhaInterpretada).
+
+% Descrição: Realiza a interpretação das linhas que possuem espaços vazios e peças
+% Detalhes: O método atom é utilizado para identificar os átomos na linha, que no caso são as peças pretas e brancas
+% Parâmetros: Lista, Lista
+expandir_linha([],[]).
+expandir_linha([X|Xs], Resultado) :-
+    number(X),
+    gerar_vazias(X, Vazias),
+    expandir_linha(Xs, Resto),
+    append(Vazias, Resto, Resultado).
+
+expandir_linha([X|Xs], [Simbolo|Resto]) :-
+    atom(X),
+    atom_string(X, S),
+    string_chars(S, [Char]),
+    atom_chars(Simbolo, [Char]),
+    expandir_linha(Xs, Resto).
+
+expandir_linha([X|Xs], [Simbolo|Resto]) :-
+    var(X),
+    term_to_atom(X, Atom),
+    atom_chars(Atom, [Char]),
+    atom_chars(Simbolo, [Char]),
+    expandir_linha(Xs, Resto).
 
 % Descrição: Função auxiliar para converter os números (1 a 8) em '.'
 % Parâmetros: Número (0 a 8), Lista
